26b4dd8dfaaa01245bcc1d4ecc9fd1edd1e99873
vaaako <vakofernanda@tutamail.com>
2026-05-18 16:10:05 -0300

    added uuid to object class

--- include/floydia/rendering/object.hpp
+++ include/floydia/rendering/object.hpp
@@ -1,28 +1,43 @@
 #pragma once
 
-#include <floydia/rendering/transform.hpp>
-
+#include "floydia/rendering/transform.hpp"
+#include <atomic>
+#include <string>
 
 namespace floyd {
-
-// Just using as reference, so this is ok
-class Renderer;
 
 // Transformable object
 class Object {
 	public:
 		Transform transform;
+		std::string name = "Object";
 
 	public:
-		Object() = default;
+		inline Object() noexcept : _uuid(Object::generate_uuid()) {}
 		virtual ~Object() = default;
+
+		// Generate new UUID and copy name
+		inline Object(const Object& other) noexcept
+			: transform(other.transform), name(other.name), _color(other._color), _uuid(Object::generate_uuid()) {}
+		// Dont copy UUID
+		Object& operator=(const Object& other) noexcept {
+			if(this != &other) {
+				this->transform = other.transform;
+				this->name = other.name;
+				this->_color = other._color;
+			}
+			return *this;
+		}
+
+		// Returns this object UUID
+		inline uint64_t uuid() const noexcept { return _uuid; }
 
 		// Color on 0-255 range
 		inline const vec4<float> color() const {
 			return (vec4<float>)this->_color * glm::vec4(255.0f);
 		}
 
-		inline void set_color(const vec4<uint8>& c) {
+		inline void set_color(const vec4<u8>& c) {
 			this->_color = (vec4<float>)c / glm::vec4(255.0f);
 		}
 
@@ -34,7 +49,14 @@ class Object {
 	private:
 		// Color is on object because it isn't considered when batching
 		vec4<float> _color = vec4<float>(1.0f);
+		u64 _uuid;
+
+		static u64 generate_uuid() noexcept {
+			static std::atomic<u64> counter = 1;
+			return counter++;
+		}
 
 };
 
 } // namespace floyd
+
